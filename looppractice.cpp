102b9402af98f54c506c45b2daa85b45c2690d4f
manishnita <99784658+manishnita@users.noreply.github.com>
2022-03-27 17:53:24 +1200

    Add files via upload

---
+++ looppractice.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+  /*int n;
+  cin>>n;
+  int i=2;
+  while ( i<n)
+  {
+  if ( n%i==0)
+  {
+    cout<<"Not Prime Number"<<endl;
+    return 0;
+  }
+  i=i+1;
+
+  }
+     cout<<"Prime"<<endl;
+     */
+    int n;
+    cin>>n;
+    int i=1;
+    while ( i<=n)
+    {
+    int j=1;
+    while(j<=n){
+        cout<<j<<" ";
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+    }
+    
+}
+ 
